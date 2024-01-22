ae30dab24bc25ae27baac87c730e9caa7e1ed8d2
LENDRA SYAPUTRA <l3noneone@gmail.com>
2024-01-23 01:59:07 +0700

    Update config.go

--- app/config/config.go
+++ app/config/config.go
@@ -13,6 +13,10 @@ var (
 	JWT_SECRET string
 )
 
+var (
+	CLD_URL string
+)
+
 type AppConfig struct {
 	DB_USERNAME string
 	DB_PASSWORD string
@@ -21,11 +25,6 @@ type AppConfig struct {
 	DB_NAME     string
 }
 
-type CldConfig struct {
-	CLDNAME     string
-	CLDKEY      string
-	CLDSECRET   string
-}
 
 func InitConfig() *AppConfig {
 	return ReadEnv()
@@ -33,7 +32,6 @@ func InitConfig() *AppConfig {
 
 func ReadEnv() *AppConfig {
 	app := AppConfig{}
-	cld := CldConfig{}
 	isRead := true
 
 	if val, found := os.LookupEnv("DBUSER"); found {
@@ -61,17 +59,8 @@ func ReadEnv() *AppConfig {
 		JWT_SECRET = val
 		isRead = false
 	}
-
-	if val, found := os.LookupEnv("CLDNAME"); found {
-		cld.CLDNAME = val
-		isRead = false
-	}
-	if val, found := os.LookupEnv("CLDKEY"); found {
-		cld.CLDKEY = val
-		isRead = false
-	}
-	if val, found := os.LookupEnv("CLDSECRET"); found {
-		cld.CLDSECRET = val
+	if val, found := os.LookupEnv("CLDURL"); found {
+		CLD_URL = val
 		isRead = false
 	}
 
@@ -86,10 +75,8 @@ func ReadEnv() *AppConfig {
 			return nil
 		}
 
+		CLD_URL = viper.GetString("CLDURL")
 		JWT_SECRET = viper.GetString("JWTSECRET")
-		cld.CLDNAME = viper.Get("CLDNAME").(string)
-		cld.CLDKEY = viper.Get("CLDKEY").(string)
-		cld.CLDSECRET = viper.Get("CLDSECRET").(string)
 		app.DB_USERNAME = viper.Get("DBUSER").(string)
 		app.DB_PASSWORD = viper.Get("DBPASS").(string)
 		app.DB_HOSTNAME = viper.Get("DBHOST").(string)
@@ -100,12 +87,9 @@ func ReadEnv() *AppConfig {
 	return &app
 }
 
-func SetupCloudinary(cldConfig *CldConfig) (*cloudinary.Cloudinary, error) {
-	cldName := cldConfig.CLDNAME
-	cldKey := cldConfig.CLDKEY
-	cldSecret := cldConfig.CLDSECRET
+func SetupCloudinary() (*cloudinary.Cloudinary, error) {
 
-	cld, err := cloudinary.NewFromParams(cldName, cldKey, cldSecret)
+	cld, err := cloudinary.NewFromURL(CLD_URL)
 	if err != nil {
 		return nil, err
 	}
