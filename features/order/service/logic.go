ef40119684896919ee690276faf0b0760d784580
lendral3n <l3noneone@gmail.com>
2024-01-24 22:49:45 +0700

    logic pengurangan stock di product ketika sudah order

--- features/order/service/logic.go
+++ features/order/service/logic.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"MyEcommerce/features/order"
+	"errors"
 )
 
 type orderService struct {
@@ -16,6 +17,17 @@ func New(repo order.OrderDataInterface) order.OrderServiceInterface {
 
 // CreateOrder implements order.OrderServiceInterface.
 func (os *orderService) CreateOrder(userIdLogin int, cartIds []uint, inputOrder order.OrderCore, items []order.OrderItemCore) (*order.OrderCore, error) {
+	if len(cartIds) == 0 {
+		return nil, errors.New("masukan barang anda")
+	}
+	if inputOrder.Address == "" {
+		return nil, errors.New("masukan alamat anda")
+	}
+
+	if inputOrder.GrossAmount == 0 {
+		return nil, errors.New("total pembayaran anda salah")
+	}
+
 	payment, err := os.orderData.InsertOrder(userIdLogin, cartIds, inputOrder, items)
 	if err != nil {
 		return nil, err
